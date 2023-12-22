9953ed90816079c54a52087107694d1b79726971
SajidChowdhury <sajidhossainchowdhury@gmail.com>
2023-12-22 14:04:35 +0530

    add shrp flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,3 +22,48 @@ TARGET_OTA_ASSERT_DEVICE := m01q
 
 # Inherit from common sdm439-common
 -include device/samsung/sdm439-common/BoardConfigCommon.mk
+
+# Device codename4
+SHRP_DEVICE_CODE := m01q
+
+# Device path
+SHRP_PATH := device/samsung/m01q
+
+# Maintainer name
+SHRP_MAINTAINER := チャンドゥ
+
+# Recovery Type (for "About" section only)
+SHRP_REC_TYPE := Treble
+
+# Device Type (for "About" section only)
+SHRP_DEVICE_TYPE := A/B
+
+# Recovery partiton
+SHRP_HAS_RECOVERY_PARTITION := true
+
+# Use this flag only if your device is A/B or Virtual A/B.
+SHRP_AB := true
+
+# Emergency DownLoad mode (0 = no EDL mode, 1 = EDL mode available)
+SHRP_EDL_MODE := 0
+
+# Internal storage path
+SHRP_INTERNAL := /sdcard
+
+# External SDcard path
+SHRP_EXTERNAL := /external_sd 
+
+# External otg path
+SHRP_OTG := /usb_otg
+
+# Flashlight: (0 = disable, 1 = enable)
+SHRP_FLASH := <0|1>
+
+# Notch
+SHRP_NOTCH := true
+
+# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock
+SHRP_EXPRESS := true
+
+## SHRP Dark mode, use this flag to have dark theme set by default
+SHRP_DARK := true
